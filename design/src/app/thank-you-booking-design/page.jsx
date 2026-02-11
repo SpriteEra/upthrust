d82c6d67790036c4c11a3efdd7b9c3e109708a75
Ajay Verma <ajayverma917034@gmail.com>
2026-02-11 13:05:42 +0530

    add thank-you-booking-design page

---
+++ design/src/app/thank-you-booking-design/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='min-h-screen flex items-center justify-center text-lg md:text-2xl 3xl:text-3xl'>Thank You Booking Design</div>
+    )
+}
+
+export default page
